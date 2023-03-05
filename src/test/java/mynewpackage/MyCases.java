b562b5512a554bd2222f7d443929f631487b7528
Raja <rajadas.rd563@gmail.com>
2023-03-05 23:35:38 +0530

    initial commit

---
+++ src/test/java/mynewpackage/MyCases.java
@@ -0,0 +1,36 @@
+package mynewpackage;
+import org.testng.annotations.Test;
+
+public class MyCases {
+	
+	@Test
+	public void testOne() {
+		System.out.println("this is my first test");
+	}
+	
+	@Test
+	public void testTwo() {
+		System.out.println("this is my second test");
+	}
+	
+	@Test
+	public void testThree() {
+		System.out.println("this is my third test");
+	}
+	
+	@Test
+	public void testFour() {
+		System.out.println("this is my fourth test");
+	}
+	
+	@Test
+	public void testFive() {
+		System.out.println("this is my fifth test");
+	}
+	
+	@Test
+	public void testSix() {
+		System.out.println("this is my sixth test");
+	}
+
+}
---
+++ src/test/java/mynewpackage/MyCasesnew.java
@@ -0,0 +1,36 @@
+package mynewpackage;
+import org.testng.annotations.Test;
+
+public class MyCasesnew {
+	
+	@Test
+	public void testOne() {
+		System.out.println("this is my first test1");
+	}
+	
+	@Test
+	public void testTwo() {
+		System.out.println("this is my second test1");
+	}
+	
+	@Test
+	public void testThree() {
+		System.out.println("this is my third test1");
+	}
+	
+	@Test
+	public void testFour() {
+		System.out.println("this is my fourth test1");
+	}
+	
+	@Test
+	public void testFive() {
+		System.out.println("this is my fifth test1");
+	}
+	
+	@Test
+	public void testSix() {
+		System.out.println("this is my sixth test1");
+	}
+
+}
